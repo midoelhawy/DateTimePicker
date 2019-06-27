8fa7c7f0d8000c864fb47978317bfee85899c185
Ahmed Hekal(MIdo elhawy) <android.com111@gmail.com>
2019-06-27 23:51:05 +0200

    Arabic

--- src/i18n/DateTimePicker-i18n.js
+++ src/i18n/DateTimePicker-i18n.js
@@ -42,6 +42,36 @@
 })(jQuery);
 
 
+/*
+	language: arabic
+	file: DateTimePicker-i18n-ar
+	author: midoelhawy (https://github.com/midoelhawy)
+*/
+
+(function ($) {
+    $.DateTimePicker.i18n["ar"] = $.extend($.DateTimePicker.i18n["ar"], {
+
+    	language: "ar",
+
+    	dateTimeFormat: "dd-MM-yyyy HH:mm",
+		dateFormat: "dd-MM-yyyy",
+		timeFormat: "HH:mm",
+
+		shortDayNames: ["احد", "اثنين", "ثلاثاء", "اربعاء", "خميس", "جمعة", "سبت"],
+		fullDayNames: ["الاحد", "الاثنين", "الثلاثاء", "الاربعاء", "الخميس", "الجمعة", "السبت"],
+		shortMonthNames: ["يناير", "فبراير", "مارس", "ابريل", "مايو", "يونيو", "يوليو", "اغسطس", "سيبتمبر", "اكتوبر", "نوفمبر", "ديسمبر"],
+		fullMonthNames: ["يناير", "فبراير", "مارس", "ابريل", "مايو", "يونيو", "يوليو", "اغسطس", "سيبتمبر", "اكتوبر", "نوفمبر", "ديسمبر"],
+
+		titleContentDate: "عين التاريخ",
+		titleContentTime: "عين الوقت",
+		titleContentDateTime: "عين التاريخ و الوقت",
+
+		setButtonContent: "تعيين",
+		clearButtonContent: "نظف"
+
+    });
+})(jQuery);
+
 
 /*
 

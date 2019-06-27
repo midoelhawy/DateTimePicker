28956d1962bc496483d1b861076d4da62dba599f
Ahmed Hekal(MIdo elhawy) <android.com111@gmail.com>
2019-06-27 23:48:52 +0200

    DateTimePicker arabic

---
+++ src/i18n/DateTimePicker-i18n-ar.js
@@ -0,0 +1,42 @@
+/* ----------------------------------------------------------------------------- 
+
+  jQuery DateTimePicker - Responsive flat design jQuery DateTime Picker plugin for Web & Mobile
+  Version 0.1.39
+  Copyright (c)2014-2019 Lajpat Shah
+  Contributors : https://github.com/nehakadam/DateTimePicker/contributors
+  Repository : https://github.com/nehakadam/DateTimePicker
+  Documentation : https://nehakadam.github.io/DateTimePicker
+
+ ----------------------------------------------------------------------------- */
+
+/*
+
+	language: arabic
+	file: DateTimePicker-i18n-ar
+	author: Mido elhawy
+	author: midoelhawy (https://github.com/midoelhawy)
+*/
+
+(function ($) {
+    $.DateTimePicker.i18n["ar"] = $.extend($.DateTimePicker.i18n["ar"], {
+        
+    	language: "ar",
+
+    	dateTimeFormat: "dd-MM-yyyy HH:mm",
+		dateFormat: "dd-MM-yyyy",
+		timeFormat: "HH:mm",
+
+		shortDayNames: ["احد", "اثنين", "ثلاثاء", "اربعاء", "خميس", "جمعة", "سبت"],
+		fullDayNames: ["الاحد", "الاثنين", "الثلاثاء", "الاربعاء", "الخميس", "الجمعة", "السبت"],
+		shortMonthNames: ["يناير", "فبراير", "مارس", "ابريل", "مايو", "يونيو", "يوليو", "اغسطس", "سيبتمبر", "اكتوبر", "نوفمبر", "ديسمبر"],
+		fullMonthNames: ["يناير", "فبراير", "مارس", "ابريل", "مايو", "يونيو", "يوليو", "اغسطس", "سيبتمبر", "اكتوبر", "نوفمبر", "ديسمبر"],
+
+		titleContentDate: "عين التاريخ",
+		titleContentTime: "عين الوقت",
+		titleContentDateTime: "عين التاريخ و الوقت",
+	
+		setButtonContent: "تعيين",
+		clearButtonContent: "نظف"
+        
+    });
+})(jQuery);
